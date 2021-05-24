b6199756ee7d09908454a2fdef7a1472bc15d6b0
doni69 <adonism10101@gmail.com>
2021-05-24 16:12:51 +1000

    kinda done

--- transformer.py
+++ transformer.py
@@ -1,58 +1,57 @@
 from greenery import fsm
 from sre_parse import parse
+from unparse import unparse
 import sre_constants as sre
-from hypothesis import given,strategies as st,note,assume,settings
+from hypothesis import given,strategies as st,note,assume,settings,example
 import re,string
 from test_regex import conservative_regex
 
-D=set("0123456789")
-W=set("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-S=set(" \t\n\r\v\f")
-ALL=set(map(chr,range(0x80))) #ASCII
+#D=set(map(ord,string.digits))
+D=set(map(ord,re.findall(r"\d","".join(map(chr,range(0x110000))))))
+#W=set(map(ord,string.ascii_letters))|D|{ord("_")}
+W=set(map(ord,re.findall(r"\w","".join(map(chr,range(0x110000))))))
+#S=set(map(ord,string.whitespace))
+S=set(map(ord,re.findall(r"\s","".join(map(chr,range(0x110000))))))
+#print(*map(len,(W,D,S)))
 
-def nfa(re):
-  def cs(op,av):
+
+#from SubPattern to greenery FSM
+def nfa(r):
+  n=fsm.epsilon([])
+  for op,av in r:
     if op is sre.LITERAL:
-      return {chr(av)}
-    if op is sre.NOT_LITERAL:
-      return ALL-{chr(av)}
-    if op is sre.ANY:
-      return ALL-{"\n"} #unless re.M
-    if op is sre.RANGE:
-      return set(map(chr,range(av[0],av[1]+1)))
-    if op is sre.CATEGORY:
-      return {
-        sre.CATEGORY_DIGIT:D,
-        sre.CATEGORY_NOT_DIGIT:ALL-D,
-        sre.CATEGORY_SPACE:S,
-        sre.CATEGORY_NOT_SPACE:ALL-S,
-        sre.CATEGORY_WORD:D|W,
-        sre.CATEGORY_NOT_WORD:ALL-D-W
-        }[av]
-    if op is sre.IN:
-      s=set()
-      neg=False
+      m=fsm.fsm({av},{0,1},0,{1},{0:{av:1}})
+    elif op in (sre.ANY,sre.NOT_LITERAL):
+      if op is sre.ANY:
+        av=ord("\n") #unless re.M
+      m=fsm.fsm({av,fsm.anything_else},{0,1},0,{1},{0:{fsm.anything_else:1}})
+    elif op is sre.IN:
+      a=set()
+      x=False
       for op,av in av:
         if op is sre.NEGATE:
-          neg=True
-          continue
-        s|=cs(op,av)
-      if neg:
-        s=ALL-s
-      return s
-  
-  a=set()
-  n=fsm.epsilon(a)
-  for op,av in re:
-    if op in (sre.LITERAL,sre.NOT_LITERAL,sre.ANY,sre.RANGE,sre.CATEGORY,sre.IN):
-      av=cs(op,av)
-      a|=av
+        	a|={fsm.anything_else}
+        	x=True
+        if op is sre.LITERAL:
+          a|={av}
+        elif op is sre.RANGE:
+        	a|=set(range(av[0],av[1]+1))
+        elif op is sre.CATEGORY:
+          a|={sre.CATEGORY_DIGIT:D,
+              sre.CATEGORY_NOT_DIGIT:D,
+              sre.CATEGORY_SPACE:S,
+              sre.CATEGORY_NOT_SPACE:S,
+              sre.CATEGORY_WORD:W,
+              sre.CATEGORY_NOT_WORD:W}[av]
+          if av in (sre.CATEGORY_NOT_DIGIT,sre.CATEGORY_NOT_SPACE,sre.CATEGORY_NOT_WORD):
+            a|={fsm.anything_else}
+            x=True
       m=fsm.fsm(a,{0,1},0,{1},
-      {0:{i:1 for i in av}})
+      {0:{fsm.anything_else:1} if x else {i:1 for i in a}})
     elif op is sre.SUBPATTERN:
       m=nfa(av[-1])
     elif op is sre.BRANCH:
-      m=fsm.null(a)
+      m=fsm.null([])
       for i in av[1]:
         m|=nfa(i)
     elif op in (sre.MAX_REPEAT,sre.MIN_REPEAT):
@@ -61,57 +60,68 @@ def nfa(re):
       if av[1] is sre.MAXREPEAT:
         u=u.star()
       else:
-        u|=fsm.epsilon(a)
+        u|=fsm.epsilon([])
         u*=av[1]-av[0]
       m+=u
     else:
       raise NotImplementedError(op,"Only true regular expressions supported :^)")
     n+=m
-  return n
-
+  return n 
+      
+#from fsm to subpattern
 def rx(n):
-  acc=-1
+  acc=-1 #accept state
   
   st = [n.initial]
   i = 0
   while i < len(st):
     c = st[i]
     if c in n.map:
-      for s in sorted(n.map[c]):
+      for s in sorted(n.map[c],key=fsm.key):
         x = n.map[c][s]
         if x not in st:
           st.append(x)
     i += 1
-  assert acc not in st
+  assert acc not in st #sanity check
+  
   brz={}
   for a in n.states:
     brz[a]={}
     for b in n.states:
-      brz[a][b]=set()
-    brz[a][acc]={""} if a in n.finals else set()
-  
+      brz[a][b]=[]
+    brz[a][acc]=[[]] if a in n.finals else []
+ 
+   
   for a in n.map:
     for s in n.map[a]:
       b=n.map[a][s]
-      brz[a][b]|={re.escape(s)}
+      if s is fsm.anything_else:
+        brz[a][b].append([(sre.IN,[(sre.NEGATE,None)]+[(sre.LITERAL,i) for i in n.alphabet if i is not fsm.anything_else])])
+      else:
+        brz[a][b].append([(sre.LITERAL,s)])
   
   for i in reversed(range(len(st))):
     a=st[i]
-    l="("+"|".join(brz[a][a])+")*" if brz[a][a] else ""
+    if len(brz[a][a])>1:
+      l=[(sre.BRANCH,(None,brz[a][a]))]
+    elif len(brz[a][a])==1:
+      l=brz[a][a][0]
+    l=[(sre.MAX_REPEAT,(0,sre.MAXREPEAT,l))] if brz[a][a] else []
     del brz[a][a]
-    for b in sorted(brz[a]):
-      brz[a][b]={l+i for i in brz[a][b]}
+    for b in brz[a]:
+      brz[a][b]=[l+i for i in brz[a][b]]
     for j in range(i):
       b=st[j]
       if brz[b][a]:
-        u="("+"|".join(brz[b][a])+")"
+        u=[(sre.BRANCH,(None,brz[b][a]))] if len(brz[b][a])>1 else brz[b][a][0]
         del brz[b][a]
         for c in brz[a]:
-          brz[b][c]|={u+i for i in brz[a][c]}
+          brz[b][c]+=[u+i for i in brz[a][c]]
       else:
         del brz[b][a]
-        
-  return "|".join(brz[a][acc])
+  
+  return [(sre.BRANCH,(None,brz[a][acc]))] if len(brz[a][acc])>1 else brz[a][acc][0] if brz[a][acc] else []
+  
 
 @given(conservative_regex())
 @settings(deadline=None)
@@ -121,7 +131,9 @@ def test_idempotence(r):
   print(cnt,repr(r))
   u=rx(nfa(parse(r)))
   note(repr(u))
-  assert u==rx(nfa(parse(u)))
+  v=rx(nfa(u))
+  note(repr(v))
+  assert u==rx(nfa(u))
 
 @given(conservative_regex(),st.data())
 @settings(deadline=None)
@@ -133,13 +145,12 @@ def test_equivalence(r,data):
     n=nfa(parse(r))
   except NotImplementedError:
     print("Not Implemented :^)")
-    return
-  u=rx(n)
+    reject()
+  u=re.compile(unparse(rx(n)))
   sr=data.draw(st.from_regex(r,fullmatch=True))
   su=data.draw(st.from_regex(u,fullmatch=True))
-  s=data.draw(st.text(alphabet=ALL))
-  assume(all(i in ALL for i in sr))
-  assume(all(i in ALL for i in su))
+  s=data.draw(st.text())
+  
   
   note(repr(r))
   note(repr(u))
@@ -154,7 +165,20 @@ def test_equivalence(r,data):
   note(re.match(u,s))
   assert bool(re.match(r,s))==bool(re.match(u,s))
 
-cnt=0
-test_idempotence()
-cnt=0
-test_equivalence()
+
+@given(conservative_regex(),conservative_regex(),st.data())
+@settings(deadline=None)
+def test_intersection(r,s,d):
+  global cnt
+  cnt+=1
+  print(cnt,r,s)
+  t=d.draw(st.text())
+  i=unparse(rx(nfa(parse(r))&nfa(parse(s))))
+  assume(i)
+  note(i[:100])
+  if re.fullmatch(r,t) and re.fullmatch(s,t):
+    note("y")
+    assert re.fullmatch(i,t)
+  else:
+  	note("n")
+  	assert not re.fullmatch(i,t)
